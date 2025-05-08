0b144a8bedc58823a0f309ab150abf8e80a12c08
marialopezsv503 <ventas.productos.sv@gmail.com>
2025-05-08 17:30:37 -0600

    segunda actualizacion repo local git

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,9 +76,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    ##'default': {
+        ## 'ENGINE': 'django.db.backends.sqlite3',
+        ## 'NAME': BASE_DIR / 'db.sqlite3',
+    ##}
+
+    ##   'default': {
+    ##    'ENGINE': 'django.db.backends.mysql',
+    ##    'NAME': 'myapp_db',
+    ##    'USER': 'root',
+    ##    'PASSWORD':'admin789',
+    ##    'HOST':'localhost',
+    ##    'PORT':'3306'
+    ## }   
+
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapp_db',
+        'USER': 'postgres',
+        'PASSWORD':'admin789',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
